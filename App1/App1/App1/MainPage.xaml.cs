e2051242cea33e463fe98da9f38d427044fa2de1
Андрiй Масло <maslo@dlit.dp.ua>
2019-02-28 21:19:04 +0200

    RegSys

---
+++ App1/App1/App1/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public void Sing_in_func(object sender, EventArgs e) {
+            int x = 0;
+            string loginS = "masloriy";
+            string login = Entry1.Text;
+            int max = 100;
+            while (login != loginS) {
+                x++;
+                if (x == max)
+                    break;
+            }
+
+            
+        }
+    }
+}
